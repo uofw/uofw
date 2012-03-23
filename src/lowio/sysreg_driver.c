7704a0a44fc3dec833e8226ddaad004d6c80d28f
jigsaw <jigsaw@80855669-be73-4f5e-9bfa-13ee22678a59>
2012-03-23 23:15:23 +0000

    sysreg_driver init commit

---
+++ src/lowio/sysreg_driver.c
@@ -0,0 +1,308 @@
+/* Copyright (C) 2011, 2012 The uOFW team
+   See the file COPYING for copying permission.
+*/
+
+#include "../global.h"
+
+static int
+sub_000001B8(int a0, int enable)
+{
+    int intr;
+    int r, t1, t2, t3;
+
+    intr = sceKernelCpuSuspendIntr();
+
+    t1 = _lw(0xBC10004C);
+    t2 = t1 & a0;
+    t3 = t1 ^ t2;
+    r = (0 < t2);
+    t1 |= a0;
+    if (enable == 0)
+        t1 = t3;
+    _sw(0xBC10004C, t1);
+
+    sceKernelCpuResumeIntr(intr);
+
+    return r;
+}
+
+/* sceSysreg_driver_47C971B2 */
+int
+sceSysregTopResetEnable(void)
+{
+    return sub_000001B8(1, 1);
+}
+
+/* sceSysreg_driver_655C9CFC */
+int
+sceSysregScResetEnable(void)
+{
+    return sub_000001B8(2, 1);
+}
+
+/* sceSysreg_driver_457FEBA9 */
+int
+sceSysregMeResetEnable(void)
+{
+    return sub_000001B8(4, 1);
+}
+
+/* sceSysreg_driver_48F1C4AD */
+int
+sceSysregMeResetDisable(void)
+{
+    return sub_000001B8(4, 0);
+}
+
+/* sceSysreg_driver_66899952 */
+int
+sceSysregAwResetEnable(void)
+{
+    return sub_000001B8(8, 1);
+}
+
+/* sceSysreg_driver_AEB8DBD1 */
+int
+sceSysregAwResetDisable(void)
+{
+    return sub_000001B8(8, 0);
+}
+
+/* sceSysreg_driver_1A27B224 */
+int
+sceSysregVmeResetEnable(void)
+{
+    return sub_000001B8(0x10, 1);
+}
+
+/* sceSysreg_driver_B73D3619 */
+int
+sceSysregVmeResetDisable(void)
+{
+    return sub_000001B8(0x10, 0);
+}
+
+/* sceSysreg_driver_0AE8E549 */
+int
+sceSysregAvcResetEnable(void)
+{
+    return sub_000001B8(0x20, 1);
+}
+
+/* sceSysreg_driver_55FF02E9 */
+int
+sceSysregAvcResetDisable(void)
+{
+    return sub_000001B8(0x20, 0);
+}
+
+/* sceSysreg_driver_30C0A141 */
+int
+sceSysregUsbResetEnable(void)
+{
+    return sub_000001B8(0x40, 1);
+}
+
+/* sceSysreg_driver_9306F27B */
+int
+sceSysregUsbResetDisable(void)
+{
+    return sub_000001B8(0x40, 0);
+}
+
+/* sceSysreg_driver_64C8E8DD */
+int
+sceSysregAtaResetEnable(void)
+{
+    return sub_000001B8(0x80, 1);
+}
+
+/* sceSysreg_driver_8CFD0DCA */
+int
+sceSysregAtaResetDisable(void)
+{
+    return sub_000001B8(0x80, 0);
+}
+
+/* sceSysreg_driver_370419AD */
+int
+sceSysregMsifResetEnable(int no)
+{
+    if (no < 0 || no >= 2)
+        return 0x80000102;
+
+    return sub_000001B8(0x100 << no, 1);
+}
+
+/* sceSysreg_driver_7DD0CBEE */
+int
+sceSysregMsifResetDisable(int no)
+{
+    if (no < 0 || no >= 2)
+        return 0x80000102;
+
+    return sub_000001B8(0x100 << no, 0);
+}
+
+/* sceSysreg_driver_C1A37B37 */
+int
+sceSysregKirkResetEnable(void)
+{
+    return sub_000001B8(0x400, 1);
+}
+
+/* sceSysreg_driver_2F9B03E0 */
+int
+sceSysregKirkResetDisable(void)
+{
+    return sub_000001B8(0x400, 0);
+}
+
+/* sceSysreg_driver_866EEB74 */
+int
+sceSysregAtahddResetEnable(void)
+{
+    return sub_000001B8(0x1000, 1);
+}
+
+/* sceSysreg_driver_9EB8C49E */
+int
+sceSysregAtahddResetDisable(void)
+{
+    return sub_000001B8(0x1000, 0);
+}
+
+/* sceSysreg_driver_4A433DC3 */
+int
+sceSysregUsbhostResetEnable(void)
+{
+    return sub_000001B8(0x2000, 1);
+}
+
+/* sceSysreg_driver_9B710D3C */
+int
+sceSysregUsbhostResetDisable(void)
+{
+    return sub_000001B8(0x2000, 0);
+}
+
+/* sceSysreg_driver_C6C75585 */
+int
+sceSysreg_driver_C6C75585(int no)
+{
+    if (no < 0 || no >= 2)
+        return 0x80000102;
+
+    return sub_000001B8(0x4000 << no, 1);
+}
+
+/* sceSysreg_driver_0995F8F6 */
+int
+sceSysreg_driver_0995F8F6(int no)
+{
+    if (no < 0 || no >= 2)
+        return 0x80000102;
+
+    return sub_000001B8(0x4000 << no, 0);
+}
+
+/* sceSysreg_driver_72887197 */
+int
+sceSysreg_driver_72887197(void)
+{
+    return sub_000001B8(1, 1);
+}
+
+/* sceSysreg_driver_32E02FDF */
+int
+sceSysreg_driver_32E02FDF(void)
+{
+    return sub_000001B8(1, 0);
+}
+
+/* sceSysreg_driver_73B3E52D */
+int
+sceSysreg_driver_73B3E52D(void)
+{
+    return (_lw(0xBC10004C) >> 16) & 1;
+}
+
+static int
+sub_000004B8(int a0, int a1)
+{
+    int intr;
+    int r, t1, t2, t3;
+
+    intr = sceKernelCpuSuspendIntr();
+
+    t1 = _lw(0xBC100050);
+    t2 = t1 & a0;
+    t3 = t1 ^ t2;
+    r = (0 < t2);
+    t1 |= a0;
+    if (enable == 0)
+        t1 = t3;
+    _sw(0xBC100050, t1);
+
+    sceKernelCpuResumeIntr(intr);
+
+    return r;
+}
+
+/* sceSysreg_driver_38527743 */
+int
+sceSysregMeBusClockEnable(void)
+{
+    return sub_000004B8(1, 1);
+}
+
+/* sceSysreg_driver_C4C21CAB */
+int
+sceSysregMeBusClockDisable(void)
+{
+    return sub_000004B8(1, 0);
+}
+
+/* sceSysreg_driver_51571E8F */
+int
+sceSysregAwRegABusClockEnable(void)
+{
+    return sub_000004B8(2, 1);
+}
+
+/* sceSysreg_driver_52B74976 */
+int
+sceSysregAwRegABusClockDisable(void)
+{
+    return sub_000004B8(2, 0);
+}
+
+/* sceSysreg_driver_44277D0D */
+int
+sceSysregAwRegBBusClockEnable(void)
+{
+    return sub_000004B8(4, 1);
+}
+
+/* sceSysreg_driver_7E1B1F28 */
+int
+sceSysregAwRegBBusClockDisable(void)
+{
+    return sub_000004B8(4, 0);
+}
+
+/* sceSysreg_driver_C2E0E869 */
+int
+sceSysregAwEdramBusClockEnable(void)
+{
+    return sub_000004B8(8, 1);
+}
+
+/* sceSysreg_driver_258782A3 */
+int
+sceSysregAwEdramBusClockDisable(void)
+{
+    return sub_000004B8(8, 0);
+}
+
+
