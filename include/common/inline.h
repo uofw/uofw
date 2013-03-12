70df512b9e404575d8bab2fb0bdd8d4bc5dc76da
Clément G <geecko.dev@free.fr>
2013-03-12 23:05:20 +0100

    correct inline.h compilation warnings, use asm __volatile

--- include/common/inline.h
+++ include/common/inline.h
@@ -6,58 +6,59 @@
 # error "Only include common_imp.h or common_header.h!"
 #endif
 
-static volatile inline s32 pspMax(s32 a, s32 b)
+inline static s32 pspMax(s32 a, s32 b)
 {
     s32 ret;
-    asm("max %0, %1, %2" : "=r" (ret) : "r" (a), "r" (b));
+    asm __volatile__ ("max %0, %1, %2" : "=r" (ret) : "r" (a), "r" (b));
     return ret;
 }
 
-static volatile inline s32 pspMin(s32 a, s32 b)
+inline static s32 pspMin(s32 a, s32 b)
 {
     s32 ret;
-    asm("min %0, %1, %2" : "=r" (ret) : "r" (a), "r" (b));
+    asm __volatile__ ("min %0, %1, %2" : "=r" (ret) : "r" (a), "r" (b));
     return ret;
 }
 
-static volatile inline void pspSync(void)
+inline static void pspSync(void)
 {
-    asm("sync");
+    asm __volatile__ ("sync");
 }
 
-static volatile inline void pspCache(char op, const void *ptr)
+inline static void pspCache(char op, const void *ptr)
 {
-    asm("cache %0, 0(%1)" : : "ri" (op), "r" (ptr));
+    asm __volatile__ ("cache %0, 0(%1)" : : "ri" (op), "r" (ptr));
 }
 
-static volatile inline void pspBreak(s32 op)
+inline static void pspBreak(s32 op)
 {
-    asm("break %0" : : "ri" (op));
+    asm __volatile__ ("break %0" : : "ri" (op));
 }
 
-static volatile inline void pspHalt(void)
+inline static void pspHalt(void)
 {
     /* The 'HALT' instruction */
-    asm(".word 0x70000000");
+    asm __volatile__ (".word 0x70000000");
 }
 
-static volatile inline s32 pspMfic(void)
+inline static s32 pspMfic(void)
 {
     s32 ret;
-    asm("mfic %0, $0" : "=r" (ret));
+    asm __volatile__ ("mfic %0, $0" : "=r" (ret));
     return ret;
 }
 
-static volatile inline s32 pspLl(s32 *ptr)
+inline static s32 pspLl(s32 *ptr)
 {
     s32 ret;
-    asm ("ll %0, (%1)" : "=r" (ret) : "r" (ptr));
+    asm __volatile__ ("ll %0, (%1)" : "=r" (ret) : "r" (ptr));
     return ret;
 }
 
-static volatile inline s32 pspSc(s32 value, s32 *ptr)
+inline static s32 pspSc(s32 value, s32 *ptr)
 {
     s32 ret = value;
-    asm ("sc %0, (%1)" : "=r" (ret) : "r" (ptr));
+    asm __volatile__ ("sc %0, (%1)" : "=r" (ret) : "r" (ptr));
     return ret;
 }
+
