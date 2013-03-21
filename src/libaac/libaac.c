d54ad5ace3c508b151466ca135a5b5d919288e18
Clément G <geecko.dev@free.fr>
2013-03-21 17:04:41 +0100

    libaac: sceAacEndEntry

--- src/libaac/libaac.c
+++ src/libaac/libaac.c
@@ -1,3 +1,7 @@
+#define SCE_AAC_MEM_SIZE (1024 * 100) // 0x19000
+
+SCE_MODULE_STOP("sceAacEndEntry");
+
 typedef struct {
 	u8 unk0[24];
 	s32 unk24;
@@ -16,7 +20,7 @@ typedef struct {
 } Unk0;
 
 static Unk0 *g_1750; // 1750
-static s32 g_max; // 1754
+static s32 g_n; // 1754
 
 void sub_00000000(s32 idx)
 {
@@ -26,16 +30,16 @@ void sub_00000000(s32 idx)
 		return;
 	}
 
-	if (g_max == 0) {
+	if (g_n == 0) {
 		return;
 	}
 
-	// ((idx < g_max) ^ 1) | (idx >> 31)
-	if (idx < 0 || idx >= g_max) {
+	// ((idx < g_n) ^ 1) | (idx >> 31)
+	if (idx < 0 || idx >= g_n) {
 		return;
 	}
 
-	p = g_1750 + idx * 1024 * 100;
+	p = g_1750 + idx * SCE_AAC_MEM_SIZE;
 
 	if (p == NULL) {
 		return;
@@ -55,4 +59,45 @@ void sub_00000000(s32 idx)
 
 	p->unk128.unk32 ^= 1;
 	p->unk128.unk36 = p->unk128.unk24 + p->unk128.unk28 * p->unk128.unk32 + 1600;
+}
+
+void sub_000000F8(void) {
+
+}
+
+// module_stop
+// sceAac_61AA42C9
+s32 sceAacEndEntry(void) {
+	int i;
+	int p;
+	s32 ret;
+
+	for (i=0, p=0; i<g_n; i++, p+=SCE_AAC_MEM_SIZE)
+		if (g_1750 == NULL) {
+			break;
+		}
+
+		if (g_n == 0) {
+			break;
+		}
+
+		if (i<0 || i>=g_n) {
+			break;
+		}
+
+		if (g_1750 + p == NULL) {
+			continue;
+		}
+
+		ret = sceAacExit(i);
+
+		if (ret < 0) {
+			return ret;
+		}
+	}
+
+	// sceAac_23D35CAE
+	sceAacTermResource();
+
+	return SCE_ERROR_OK;
 }
