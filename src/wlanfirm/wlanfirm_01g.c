612b591433d6fdb2ddf24e19d04f072cda6978f6
Clément G <geecko.dev@free.fr>
2013-03-26 16:04:52 +0100

    wlanfirm: more corrections

--- src/wlanfirm/wlanfirm_01g.c
+++ src/wlanfirm/wlanfirm_01g.c
@@ -8,24 +8,24 @@
 #include "firmware/magpie.c"
 #include "firmware/magpie_helper.c"
 
-s32 module_start()
+s32 module_start(SceSize argc __attribute__((unused)), void *argp __attribute__((unused)))
 {
-	u16 key45;
+    u16 key45;
 
-	// sceIdStorage_driver_6FE062D1
-	sceIdStorageLookup(0x45, 0, &key45, sizeof(u16));
+    // sceIdStorage_driver_6FE062D1
+    sceIdStorageLookup(0x45, 0, &key45, sizeof(u16));
 
-	if((key45 & 0xF000) != 0)
-	{
-	    return 1;
-	}
+    if ((key45 & 0xF000) != 0)
+    {
+        return 1;
+    }
 
-	sceWlanDrv_driver_1747351B(
-		wlanfirmHelper, // 0x14C
-		wlanfirmHelperSize, // 1852
-		wlanfirm, // 0x888
-		wlanfirmSize, // 87168
-	);
+    sceWlanDrv_driver_1747351B(
+        wlanfirmHelper, // 0x14C
+        wlanfirmHelperSize, // 1852
+        wlanfirm, // 0x888
+        wlanfirmSize, // 87168
+    );
 
-	return 0;
+    return 0;
 }
