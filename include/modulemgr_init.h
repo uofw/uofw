90f2e45a45de1099e68138e1b98a9320aa50bcad
Felix <FelixDev91@gmail.com>
2013-04-13 14:27:01 +0200

    include/modulemgr_init.h: Added SceInitApiType enumeration and more functions.

--- include/modulemgr_init.h
+++ include/modulemgr_init.h
@@ -5,17 +5,52 @@
 #include "common_header.h"
 
 enum SceApplicationType {
-    SCE_INIT_APPLICATION_VSH = 0x100,      
-    SCE_INIT_APPLICATION_UPDATER = 0x110, /* Might be incorrect. */
-    SCE_INIT_APPLICATION_GAME = 0x200,
-    SCE_INIT_APPLICATION_POPS = 0x300,
+    SCE_INIT_APPLICATION_VSH        = 0x100,      
+    SCE_INIT_APPLICATION_UPDATER    = 0x110, /* Might be incorrect. */
+    SCE_INIT_APPLICATION_GAME       = 0x200,
+    SCE_INIT_APPLICATION_POPS       = 0x300,
 };
 
-int sceKernelSetInitCallback(void *, int, int);
+enum SceInitApiType {
+    SCE_INIT_APITYPE_NPDRM_MS       = 0x116,
+    SCE_INIT_APITYPE_NPDRM_EF       = 0x118,
+    SCE_INIT_APITYPE_DISC           = 0x120,
+    SCE_INIT_APITYPE_DISC_UPDATER   = 0x121,
+    SCE_INIT_APITYPE_DISC_DEBUG     = 0x122,
+    SCE_INIT_APITYPE_DISC_EMU_MS1   = 0x123, /* np9660 game */
+    SCE_INIT_APITYPE_DISC_EMU_EF1   = 0x125,
+    SCE_INIT_APITYPE_USBWLAN        = 0x130, /* Game sharing */
+    SCE_INIT_APITYPE_USBWLAN_DEBUG  = 0x131,
+    SCE_INIT_APITYPE_MS1            = 0x140,
+    SCE_INIT_APITYPE_MS2            = 0x141,
+    SCE_INIT_APITYPE_MS3            = 0x142,
+    SCE_INIT_APITYPE_MS4            = 0x143, /* comic reader */
+    SCE_INIT_APITYPE_MS5            = 0x144, /* pops */
+    SCE_INIT_APITYPE_MS6            = 0x145,
+    SCE_INIT_APITYPE_EF1            = 0x151,
+    SCE_INIT_APITYPE_EF2            = 0x152,
+    SCE_INIT_APITYPE_EF3            = 0x153,
+    SCE_INIT_APITYPE_EF4            = 0x154,
+    SCE_INIT_APITYPE_EF5            = 0x155,
+    SCE_INIT_APITYPE_EF6            = 0x156,
+    SCE_INIT_APITYPE_MLNAPP_MS      = 0x170,
+    SCE_INIT_APITYPE_MLNAPP_EF      = 0x171,
+    SCE_INIT_APITYPE_KERNEL_1       = 0x200,
+    SCE_INIT_APITYPE_VSH_1          = 0x210, /* ExitGame */
+	SCE_INIT_APITYPE_VSH_2          = 0x220, /* ExitVSH */
+    SCE_INIT_API_TYPE_KERNEL_REBOOT = 0x300,
+};
+
 int sceKernelApplicationType(void);
+int sceKernelInitApitype(void);
 
 int sceKernelRegisterChunk(int chunkId, int unk1);
-int sceKernelGetChunk(int chunkId);
+SceUID sceKernelGetChunk(int chunkId);
+
+s32 sceKernelBootFrom(void);
+int sceKernelSetInitCallback(void *, int, int);
 
 void *InitForKernel_040C934B(void);
+void *InitForKernel_D83A9BD7(void *);
+s32 InitForKernel_9D33A110(void);
 
