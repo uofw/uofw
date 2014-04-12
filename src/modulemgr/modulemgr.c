e9d60ca915408f68bc6a5396c64c4f08087b2a54
Felix-Dev <FelixDev91.gmail.com>
2014-04-12 21:42:28 +0200

    src/modulemgr: Finished ModuleMgrInit().

--- src/modulemgr/modulemgr.c
+++ src/modulemgr/modulemgr.c
@@ -10,6 +10,13 @@ typedef struct {
     SceUID threadId; // 0
     SceUID semaId; // 4
     SceUID eventId; // 8
+    SceUID userThreadId; // 12
+    u32 unk16;
+    u32 unk20;
+    u32 unk24;
+    u32 unk28;
+    u32 unk32;
+    u32 unk36;
 } SceModuleManagerCB;
 
 SCE_MODULE_INFO("sceModuleManager", SCE_MODULE_KIRK_MEMLMD_LIB | SCE_MODULE_KERNEL | SCE_MODULE_ATTR_CANT_STOP | 
@@ -20,15 +27,28 @@ SCE_SDK_VERSION(SDK_VERSION);
 
 SceModuleManagerCB g_ModuleManager; // 0x00009A20
 
-s32 ModuleMgrInit(SceSize argc __attribute__((unused)), void *argp) 
+// 0x00000178
+static void exe_thread(SceSize args, void *argp)
 {
-    g_ModuleManager.threadId = sceKernelCreateThread("SceKernelModmgrWorker", (void *)exe_thread, SCE_KERNEL_MODULE_INIT_PRIORITY, 
-            0x4000, 0, NULL); // 0x00005078
-	g_ModuleManager.semaId = sceKernelCreateSema("SceKernelModmgr", 0, 1, 1, NULL);
+    
+}
+
+// 0x00005048
+s32 ModuleMgrInit(SceSize argc __attribute__((unused)), void *argp __attribute__((unused))) 
+{
+    g_ModuleManager.threadId = sceKernelCreateThread("SceKernelModmgrWorker", (SceKernelThreadEntry)exe_thread, 
+            SCE_KERNEL_MODULE_INIT_PRIORITY, 0x4000, 0, NULL); // 0x00005078
+	g_ModuleManager.semaId = sceKernelCreateSema("SceKernelModmgr", 0, 1, 1, NULL); // 0x0000509C
 
 	g_ModuleManager.eventId = sceKernelCreateEventFlag("SceKernelModmgr", SCE_EVENT_WAITAND, 0, 0); // 0x000050B8
-	g_ModuleManager.user_thread = -1;
-	g_ModuleManager.unk2 = (SceUID *)&modMgrCB.unk2;
+    
+    g_ModuleManager.userThreadId = -1; // 0x000050DC
+    g_ModuleManager.unk16 = -1; // 0x000050D0
+    
+    g_ModuleManager.unk20 = &g_ModuleManager.unk20; // 0x000050D8
+    g_ModuleManager.unk24 = &g_ModuleManager.unk20; //0x000050F0
+    g_ModuleManager.unk32 = 0; // 0x000050E0
+    g_ModuleManager.unk36 = 0; // 0x000050D4
     
 	return SCE_KERNEL_RESIDENT;
 }
