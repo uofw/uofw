638a9ff3258c777926b835e13c1573b90b1e3924
Kern <wals0323@umn.edu>
2014-05-15 21:57:31 -0500

    include/dmacman.h: adding more information to SceDmaOp struct

--- include/dmacman.h
+++ include/dmacman.h
@@ -5,21 +5,24 @@
 #include "common_header.h"
 
 typedef struct {
-    void *unk0;
-    u32 unk4;
-    u32 unk8;
-    SceDmaOp *next;
+    SceDmaOp *unk0; // Prev (for unrelated ops)
+    SceDmaOp *unk4; // Next (for unrelated ops)
+    SceDmaOp *unk8; // Prev (for linked ops)
+    SceDmaOp *next; // Next (for linked ops)
     u32 unk16;
     u32 unk20;
     int (*)(int, int) callback;
-    u16 unk28;
+    u16 unk28;      // Status
     u16 unk30;
     u32 unk32;
     u32 unk36;
     u32 unk40;
     u32 unk44;
     u32 unk48;
+    u16 unk52;      // id?
+    u16 unk54;      // num ops in link
     u32 unk56;
+    u32 unk60;
 } SceDmaOp;
 
 int sceKernelDmaOpQuit(SceDmaOp*);
