606617cb0eed8d44f1cbc84cc5245d54da5f4673
uOFW Doc builder <travis@example.com>
2015-05-27 18:50:05 +0000

    Rebuilt docs for cb3abcdb91d3db7a99ee05db3923779886b053fe

--- search/all_63.js
+++ search/all_63.js
@@ -4,6 +4,7 @@ var searchData=
   ['callback',['callback',['../structSceSysconPacket.html#a8ab8381341fe6e81b768db49d0431842',1,'SceSysconPacket']]],
   ['cbid',['cbId',['../structSceGeDisplayList.html#ab585e75f900c18733ffef47ec44be9e7',1,'SceGeDisplayList']]],
   ['configfile',['configFile',['../structSceLoadCoreBootInfo.html#a120a5e3cf0a7f7b500c3639f57a9e330',1,'SceLoadCoreBootInfo::configFile()'],['../structSceKernelLoadExecVSHParam.html#a98ef52a6f7c4d189ecfce3d03a8b1824',1,'SceKernelLoadExecVSHParam::configFile()']]],
+  ['copyinputdata',['copyInputData',['../structSceCtrlInputDataTransferHandler.html#aba7f069a5f49d6654eef9496ab294a83',1,'SceCtrlInputDataTransferHandler']]],
   ['countregval',['countRegVal',['../structSceModule.html#a9ebe35022bb1ac98215270cd202dd162',1,'SceModule']]],
   ['ctx',['ctx',['../structSceGeContext.html#a8eb167ba964c2fb7eeecb24a954d0ec9',1,'SceGeContext::ctx()'],['../structSceGeListArgs.html#af92b45f3f182b223c84457ef36e48d49',1,'SceGeListArgs::ctx()'],['../structSceGeDisplayList.html#ac80233abc98bad636aba0488a311b84b',1,'SceGeDisplayList::ctx()']]],
   ['ctxuptodate',['ctxUpToDate',['../structSceGeDisplayList.html#a6a1cc8a3682e6d61a8f21fd451d3eca0',1,'SceGeDisplayList']]],
--- search/all_66.js
+++ search/all_66.js
@@ -5,6 +5,5 @@ var searchData=
   ['finish_5farg',['finish_arg',['../structSceGeCallbackData.html#a6e215f92fb651891facbc6237ec1f1d3',1,'SceGeCallbackData']]],
   ['finish_5ffunc',['finish_func',['../structSceGeCallbackData.html#a7a6258ada21d130374caa6a3785c2fa2',1,'SceGeCallbackData']]],
   ['flags',['flags',['../structSceGeDisplayList.html#a005a4a816dc80af9e7ca7c891b6937d4',1,'SceGeDisplayList::flags()'],['../structSceKernelLoadExecVSHParam.html#a5ac94f26acc0f7e203eb97dbb49d2472',1,'SceKernelLoadExecVSHParam::flags()']]],
-  ['func',['func',['../structSceCtrlUnkStruct.html#a8e62d10c00d23456424466bc0836def8',1,'SceCtrlUnkStruct']]],
   ['functablesize',['funcTableSize',['../structSceSyscallTable.html#a2b1d37ae6c258e8872e93d674ef58c43',1,'SceSyscallTable']]]
 ];
