3abfa10f791c6bb346fdbea3c47043f866461665
John Kelley <john@kelley.ca>
2021-01-17 15:06:46 -0500

    Incorporate PR feedback for tautological compares

--- src/syscon/syscon.c
+++ src/syscon/syscon.c
@@ -2043,8 +2043,10 @@ s32 sceSysconBatteryGetInfo(s32 *info)
 s32 sceSysconGetBattVolt(s32 *volt)
 {   
     u8 version =_sceSysconGetBaryonVersion() >> 16;
-    if ((version & 0xF0) != 0x20 &&
-      (version & 0xFF) != 0x2A &&
+    /* Note Sony also does the following tautological check:
+         if ((version & 0xF0) == 0x29)
+    */
+    if ((version & 0xFF) != 0x2A &&
       (version & 0xF0) != 0x30 &&
       (version & 0xF0) != 0x40)
         return 0x80000004;
@@ -2059,8 +2061,10 @@ s32 sceSysconGetBattVolt(s32 *volt)
 s32 sceSysconGetBattVoltAD(s32 *volt1, s32 *volt2)
 {
     u8 version = _sceSysconGetBaryonVersion() >> 16;
-    if ((version & 0xF0) != 0x20 &&
-      (version & 0xFF) != 0x2A &&
+    /* Note Sony also does the following tautological check:
+         if ((version & 0xF0) == 0x29)
+    */
+    if ((version & 0xFF) != 0x2A &&
       (version & 0xF0) != 0x30 &&
       (version & 0xF0) != 0x40)
         return 0x80000004;
@@ -2172,8 +2176,10 @@ s32 sceSysconBatteryGetChargeTime(s32 *time)
 s32 _sceSysconBatteryCommon(u32 cmd, s32 *ptr)
 {
     u32 version = _sceSysconGetBaryonVersion() >> 16;
-    if ((version & 0xF0) == 0x20 ||
-     (version & 0xFF) == 0x2A ||
+    /* Note Sony also does the following tautological check:
+         if ((version & 0xF0) == 0x29)
+    */
+    if ((version & 0xFF) == 0x2A ||
      (version & 0xF0) == 0x30 ||
      (version & 0xF0) == 0x40)
         return 0x80000004;
