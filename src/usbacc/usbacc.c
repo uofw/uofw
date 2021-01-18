9a056effe39e5991aa29af9923db07bfe5666fd0
joel16 <joel16@iastate.edu>
2021-01-18 14:32:10 -0500

    usbacc: Implement sceUsbAcc_internal_2A100C1F

--- src/usbacc/usbacc.c
+++ src/usbacc/usbacc.c
@@ -132,19 +132,20 @@ int sceUsbBus_driver_8A3EB5D2(void);
 
 // Globals
 struct UsbDriver g_drv;     // 0x00000CC4
-u8 unk0;                    // 0x00000D53
-u16 unk1;                   // 0x00000D50
-u8 unk2;                    // 0x00000D52
+u8 g_unk0;                  // 0x00000D53
+u16 g_unk1;                 // 0x00000D50
+u8 g_unk2;                  // 0x00000D52
+u8 g_unk3;                  // 0x00000D53
 struct UsbdDeviceReq g_req; // 0x00000CAC
 
-// Subroutine sceUsbAccGetAuthStat - Address 0x00000000 - Aliases: sceUsbAcc_79A1C743, sceUsbAcc_driver_79A1C743
+// Subroutine sceUsbAccGetAuthStat - Address 0x00000000 - Aliases: sceUsbAcc_79A1C743, sceUsbAcc_driver_79A1C743 -- Done
 // Exported in sceUsbAcc_internal, sceUsbAcc and sceUsbAcc_driver
 s32 sceUsbAccGetAuthStat(void)
 {
     int intr = sceKernelCpuSuspendIntr();
     s32 ret = SCE_ERROR_USB_BUS_DRIVER_NOT_STARTED;
     
-    if (unk0) {
+    if (g_unk0) {
         ret = 0;
         if (sceUsbBus_driver_8A3EB5D2() == 0)
             ret = 0x80243701;
@@ -161,11 +162,24 @@ s32 sceUsbAccGetInfo(void)
     return 0;
 }
 
-// Subroutine sceUsbAcc_internal_2A100C1F - Address 0x00000154
+// Subroutine sceUsbAcc_internal_2A100C1F - Address 0x00000154 -- TODO: Verify
 // Exported in sceUsbAcc_internal
-s32 sceUsbAcc_internal_2A100C1F(void)
+s32 sceUsbAcc_internal_2A100C1F(struct UsbdDeviceReq *arg)
 {
-    return 0;
+    s32 ret = SCE_ERROR_USB_BUS_DRIVER_NOT_STARTED;
+    
+    if (g_unk3) {
+        ret = SCE_ERROR_USB_INVALID_ARGUMENT;
+        
+        if (*(u8 *)(&arg[1] + 3) < 0x3D) {
+            sceKernelDcacheWritebackRange(&arg[1], sizeof(arg[2]));
+            arg[0] = g_req;
+            arg[2] = *((&arg[1] + 3) + 4);
+            ret = sceUsbbdReqSend(arg);
+        }
+    }
+    
+    return ret;
 }
 
 // Subroutine sceUsbAccRegisterType - Address 0x000004AC -- Done
@@ -174,9 +188,9 @@ s32 sceUsbAccRegisterType(u16 type)
 {
     s32 error = SCE_ERROR_USB_DRIVER_NOT_FOUND;
 
-    if ((unk1 & type) == 0) {
+    if ((g_unk1 & type) == 0) {
         error = 0;
-        unk1 = unk1 | type;
+        g_unk1 = g_unk1 | type;
     }
     
     return error;
@@ -188,9 +202,9 @@ s32 sceUsbAccUnregisterType(u16 type)
 {
     s32 error = SCE_ERROR_USB_DRIVER_NOT_FOUND;
     
-    if ((unk1 & type) != 0) {
+    if ((g_unk1 & type) != 0) {
         error = 0;
-        unk1 = unk1 & ~type;
+        g_unk1 = g_unk1 & ~type;
     }
     
     return error;
@@ -201,8 +215,8 @@ s32 module_start(SceSize args __attribute__((unused)), void *argp __attribute__(
 {
     s32 ret = 0;
     if ((ret = sceUsbbdRegister(&g_drv)) >= 0) {
-        unk1 = 0;
-        unk2 = 0;
+        g_unk1 = 0;
+        g_unk2 = 0;
     }
     
     return (ret < 0)? 1: 0;
