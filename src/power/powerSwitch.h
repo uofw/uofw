5bae17f872b4fea0225c2358021bcdfe845ae36e
Felix-Dev <FelixDev91@gmail.com>
2021-02-12 23:49:40 +0100

    Add power to global makefile.

--- src/power/powerSwitch.h
+++ src/power/powerSwitch.h
@@ -7,7 +7,7 @@
 
 #include <common_header.h>
 
-// TODO: I need theese two fucntions in src/power/powerSwitch.c alongside power_kernel.h. However, I can't
+// I need theese two functions in src/power/powerSwitch.c alongside power_kernel.h. However, I can't
 // include both power_kernel.h and power_user.h in that file, so I am using this header
 // here as a workaround.
 
