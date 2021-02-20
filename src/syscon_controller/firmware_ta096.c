9e95472d5e6a893750be2bb79508b9e3865eac65
Felix-Dev <FelixDev91@gmail.com>
2021-02-20 08:57:36 +0100

    Small changes.

--- src/syscon_controller/firmware_ta096.c
+++ src/syscon_controller/firmware_ta096.c
@@ -5,8 +5,10 @@
 /*
  *
  *          SYSCON controller firmware
+ * 
+ *				Motherboard TA-096
  *
- *       Renesas/NEC 78K0 - model D78F0534
+ *       Renesas/NEC 78K0KE2 - model D78F0534
  *
  */
 
@@ -26,14 +28,14 @@
  * This base length represents the first three members in the ::SceSysconPacket.tx[16] data buffer, which
  * are always sent back to the SYSCON module.
  */
-#define SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH	3
+#define SYSCON_CMD_TRANSMIT_DATA_BASE_LEN	3
 
 const u32 g_baryonVersion = 0x00403000; // 0x010E
 
 #define SYSCON_TIMESTAMP_DATE_PREFIX_LEN	8
 #define SYSCON_TIMESTAMP_LEN				12
 
-u8 g_buildDate[] = "$Date:: 2011-05-09 20:45:25 +0900#$"; // 0x0112
+const u8 g_buildDate[] = "$Date:: 2011-05-09 20:45:25 +0900#$"; // 0x0112
 
 void (*g_sysconCmdGetOps[])(void) = {
 	exec_syscon_cmd_nop,
@@ -92,33 +94,30 @@ u8 g_unkFED3; // 0xFED3
 
 /* Special Function Registers (SFR) */
 
-#define GET_PORT_VAL(i)		*(u8 *)(0xFF00 + i)
-#define SET_PORT_VAL(i, v)	*(u8 *)(0xFF00 + i) = (v)
+#define GET_PORT_DATA(i)		*(u8 *)(0xFF00 + (i))
+#define SET_PORT_DATA(i, v)		*(u8 *)(0xFF00 + (i)) = (v)
 
-#define GET_SIO10_VAL		*(u8 *)0xFF0F	/* Serial I/O shift register 10 */
+#define GET_SIO10_DATA			*(u8 *)0xFF0F	/* Serial I/O shift register 10 */
 
 /* functions */
 
 // sub_0660
 void RESET(void)
 {
-
 }
 
 // sub_06D6
 void rotate_and_swap()
 {
-
 }
 
 // sub_0725
 void xor_bytes()
 {
-
 }
 
 // sub_073A
-void sub_073A()
+void sub_073A(void)
 {
 	return;
 }
@@ -126,112 +125,147 @@ void sub_073A()
 // sub_073B
 void sub_073B()
 {
-
 }
 
 // sub_075D
 void sub_075D()
 {
-
 }
 
 // sub_075F
 void main(void)
 {
-
 }
 
 // sub_0818
 void init_devices_1()
 {
-
 }
 
 // sub_0932
 void sub_0932(void)
 {
-
 }
 
 // sub_0974
 void sub_0974(void)
 {
-
 }
 
 // sub_987
 void sub_0987(void)
 {
-
 }
 
 // sub_099A
 void sub_099A()
 {
-
 }
 
 //sub_09E1
 void init_allegrex(void)
 {
-
 }
 
 //sub_0A21
 void sub_0A21(void)
 {
-
 }
 
 // sub_0A5E
 void sub_0A5E(void)
 {
-
 }
 
 // sub_0A97
 void sub_0A97(void)
 {
-
 }
 
 // sub_0AB9
 void start_allegrex_reset(void)
 {
-
 }
 
 // sub_0AF1
 void init_allegrex_3(void)
 {
-
 }
 
 // sub_0B58
 void sub_0B58(void)
 {
-
 }
 
 // sub_0B5B
 void sub_0B5B(void)
 {
-
 }
 
 // sub_0B73
 void poll_intrerfaces(void)
 {
-
 }
 
-// TODO: More functions here
+// sub_10A2
+void sub_10A2(void)
+{
+}
+
+// sub_10Ac
+void INTWT(void)
+{
+}
+
+// sub_1272
+void INTWTI(void)
+{
+}
+
+// sub_1458
+void INTTM51(void)
+{
+}
+
+// sub_146D
+void INTTMH0(void)
+{
+}
+
+// sub_14A0
+void INTPO(void)
+{
+}
+
+// sub_14C2
+void INTP1(void)
+{
+}
+
+// sub_14DA
+void INTP2(void)
+{
+}
+
+// sub_14F5
+void INTP5(void)
+{
+}
+
+// sub_1502
+void sub_1502(void)
+{
+}
+
+// sub_1527
+void sub_1527(void)
+{
+}
 
 // sub_1536
 /* Interrupt handler for serial interface CSI10. Called when a CSI10 communication has ended. */
 void INTCSI10(void)
 {
-
 }
 
 /* SYSCON [get] commands */
@@ -239,14 +273,14 @@ void INTCSI10(void)
 // sub_1921
 void exec_syscon_cmd_nop(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN;
 	g_curSysconCmdId = PSP_SYSCON_CMD_NOP;
 }
 
 // sub_1928
 void exec_syscon_cmd_get_baryon_version(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 4;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 4;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_BARYON_VERSION;
 
 	((u32 *)g_transmitData)[0] = g_baryonVersion;
@@ -258,7 +292,7 @@ void transmit_data_set_digital_user_key_data(void)
 	// PORT 7 seems to contain state values for the following buttons:
 	//		UP, DOWN, LEFT, RIGHT
 	//		TRIANGLE, CIRCLE, CROSS, SQUARE
-	g_transmitData[0] = GET_PORT_VAL(7);
+	g_transmitData[0] = GET_PORT_DATA(7);
 
 
 	// Port 4 seems to contain state values for the following buttons:
@@ -268,7 +302,7 @@ void transmit_data_set_digital_user_key_data(void)
 	// Port 2 seems to contain state values for the following buttons:
 	//	SCE_CTRL_INTERCEPTED
 	//  SCE_CTRL_HOLD
-	u8 ctrlData2 = ((GET_PORT_VAL(2) << 4) & 0x30) | (GET_PORT_VAL(4) & 0xF);
+	u8 ctrlData2 = ((GET_PORT_DATA(2) << 4) & 0x30) | (GET_PORT_DATA(4) & 0xF);
 	ctrlData2 |= 0x40; // Set 7th bit -- SCE_CTRL_WLAN_UP?
 	ctrlData2 &= ~0x80; // clear 8th bit -- SCE_CTRL_REMOTE?
 
@@ -282,7 +316,7 @@ void transmit_data_set_digital_kernel_key_data(void)
 	// VolUp, VolDown, Screen, Note (0xF)
 	// Disc, MS, 0x08000000 (unknown) - 0xB
 
-	g_transmitData[2] = GET_PORT_VAL(5) | 0xFC; // 1957
+	g_transmitData[2] = GET_PORT_DATA(5) | 0xFC; // 1957
 
 	//u8 isDiscPresent = (((g_unkFE45 >> 2) & 0x1) ^ 0xFF) & 0x1;
 	u8 isDiscPresent = !((g_unkFE45 >> 2) & 0x1);
@@ -316,7 +350,7 @@ void transmit_data_set_digital_kernel_key_data(void)
 // sub_197A
 void exec_syscon_cmd_get_digital_key(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 2;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 2;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_DIGITAL_KEY;
 
 	transmit_data_set_digital_user_key_data();
@@ -325,7 +359,7 @@ void exec_syscon_cmd_get_digital_key(void)
 // sub_1984
 void exec_syscon_cmd_get_analog(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 2;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 2;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_ANALOG;
 
 	g_transmitData[0] = g_ctrlAnalogDataX;
@@ -335,7 +369,7 @@ void exec_syscon_cmd_get_analog(void)
 // sub_1995
 void exec_syscon_cmd_get_tachyon_temp(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 2;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 2;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_TACHYON_TEMP;
 
 	/* This command has been retired in later firmwares. */
@@ -346,7 +380,7 @@ void exec_syscon_cmd_get_tachyon_temp(void)
 // sub_19A0
 void exec_syscon_cmd_get_digital_key_analog(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 4;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 4;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_DIGITAL_KEY_ANALOG;
 
 	transmit_data_set_digital_user_key_data();
@@ -358,7 +392,7 @@ void exec_syscon_cmd_get_digital_key_analog(void)
 // sub_19B4
 void exec_syscon_cmd_get_kernel_digital_key(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 4;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 4;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_KERNEL_DIGITAL_KEY;
 
 	transmit_data_set_digital_user_key_data();
@@ -368,7 +402,7 @@ void exec_syscon_cmd_get_kernel_digital_key(void)
 // sub_19C1
 void exec_syscon_cmd_get_kernel_digital_key_analog(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 6;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 6;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_KERNEL_DIGITAL_KEY_ANALOG;
 
 	transmit_data_set_digital_user_key_data();
@@ -381,7 +415,7 @@ void exec_syscon_cmd_get_kernel_digital_key_analog(void)
 // sub_19D8
 void exec_syscon_cmd_read_clock(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 4;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 4;
 	g_curSysconCmdId = PSP_SYSCON_CMD_READ_CLOCK;
 
 	((u32 *)g_transmitData)[0] = g_clock;
@@ -390,7 +424,7 @@ void exec_syscon_cmd_read_clock(void)
 // sub_19E7
 void exec_syscon_cmd_read_alarm(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 4;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 4;
 	g_curSysconCmdId = PSP_SYSCON_CMD_READ_ALARM;
 
 	((u32 *)g_transmitData)[0] = g_alarm;
@@ -399,7 +433,7 @@ void exec_syscon_cmd_read_alarm(void)
 // sub_19F6
 void exec_syscon_cmd_get_power_supply_status(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 1;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 1;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_POWER_SUPPLY_STATUS;
 
 	g_transmitData[0] = g_powerSupplyStatus;
@@ -408,7 +442,7 @@ void exec_syscon_cmd_get_power_supply_status(void)
 // sub_1A01
 void exec_syscon_cmd_get_tachyon_wdt_status(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 1;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 1;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_TACHYON_WDT_STATUS;
 
 	g_transmitData[0] = g_watchdogTimerStatus;
@@ -417,7 +451,7 @@ void exec_syscon_cmd_get_tachyon_wdt_status(void)
 // sub_1A0D
 void exec_syscon_cmd_get_batt_volt(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 1;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 1;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_BATT_VOLT;
 
 	g_transmitData[0] = g_battVoltBase;
@@ -426,7 +460,7 @@ void exec_syscon_cmd_get_batt_volt(void)
 // sub_1A19
 void exec_syscon_cmd_get_wake_up_factor(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 2;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 2;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_WAKE_UP_FACTOR;
 
 	((u16 *)g_transmitData)[0] = g_wakeUpFactor;
@@ -435,7 +469,7 @@ void exec_syscon_cmd_get_wake_up_factor(void)
 // sub_1A2A
 void exec_syscon_cmd_get_wake_up_req(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 1;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 1;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_WAKE_UP_REQ;
 
 	/* This command appears to be retired now. */
@@ -445,7 +479,7 @@ void exec_syscon_cmd_get_wake_up_req(void)
 // sub_1A34
 void exec_syscon_cmd_get_baryon_status_2(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 1;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 1;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_STATUS2;
 
 	g_transmitData[0] = g_baryonStatus2; // 0x1A3C
@@ -458,7 +492,7 @@ void exec_syscon_cmd_get_baryon_status_2(void)
 // sub_1A43
 void exec_syscon_cmd_get_timestamp(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + SYSCON_TIMESTAMP_LEN;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + SYSCON_TIMESTAMP_LEN;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_TIMESTAMP;
 
 	/*
@@ -487,7 +521,7 @@ void exec_syscon_cmd_get_timestamp(void)
 // sub_1A88
 void exec_syscon_cmd_get_video_cable(void)
 {
-	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LENGTH + 1;
+	g_sysconCmdTransmitDataLength = SYSCON_CMD_TRANSMIT_DATA_BASE_LEN + 1;
 	g_curSysconCmdId = PSP_SYSCON_CMD_GET_VIDEO_CABLE;
 
 	/* No video cable support for TA-096. */
@@ -533,7 +567,7 @@ void response_handler()
 // TODO: more functions here...
 
 // sub_5011
-void memcpy(void *pSrc, void *pDst, u16 n)
+void memcpy(const void *pSrc, void *pDst, u16 n)
 {
 	while (n-- != 0)
 	{
@@ -561,7 +595,7 @@ u8 memcmp(const void *s1, const void *s2, u16 n)
 
 // sub_5076
 // Pairwise XOR's the first 10 bytes of s1 with s2 and overwrites the first 10 bytes s2 with the resulting bytes
-void xorloop_0x10(void *s1, void *s2)
+void xorloop_0x10(const void *s1, void *s2)
 {
 	for (int i = 0xF; i >= 0; i--)
 	{
