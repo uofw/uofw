df008a980f0bde618f80e1c016d07094a9101ae9
Arthur Blot <arthur.blot78@gmail.com>
2022-05-21 18:42:19 +0200

    dmacman: fix compilation/audio

--- include/dmacman.h
+++ include/dmacman.h
@@ -4,14 +4,14 @@
 
 #include "common_header.h"
 
-typedef struct {
-    SceDmaOp *unk0; // Prev (for unrelated ops)
-    SceDmaOp *unk4; // Next (for unrelated ops)
-    SceDmaOp *unk8; // Prev (for linked ops)
-    SceDmaOp *next; // Next (for linked ops)
+typedef struct SceDmaOp {
+    struct SceDmaOp *unk0; // Prev (for unrelated ops)
+    struct SceDmaOp *unk4; // Next (for unrelated ops)
+    struct SceDmaOp *unk8; // Prev (for linked ops)
+    struct SceDmaOp *next; // Next (for linked ops)
     u32 unk16;
     u32 unk20;
-    int (*)(int, int) callback;
+    int (*callback)(int, int);
     u16 unk28;      // Status
     u16 unk30;
     u32 unk32;
@@ -26,12 +26,12 @@ typedef struct {
 } SceDmaOp;
 
 int sceKernelDmaOpQuit(SceDmaOp*);
-int sceKernelDmaOpAssign(u32*, int, int, int, int);
-int sceKernelDmaOpSetCallback(u32*, int (*)(int, int), int);
-int sceKernelDmaOpSetupLink(u32*, int, u32*);
-int sceKernelDmaOpEnQueue(u32*);
-int sceKernelDmaOpDeQueue(u32*);
-u32 *sceKernelDmaOpAlloc(void);
-s32 sceKernelDmaOpFree(u32*);
+int sceKernelDmaOpAssign(SceDmaOp*, int, int, int, int);
+int sceKernelDmaOpSetCallback(SceDmaOp*, int (*)(int, int), int);
+int sceKernelDmaOpSetupLink(SceDmaOp*, int, u32*);
+int sceKernelDmaOpEnQueue(SceDmaOp*);
+int sceKernelDmaOpDeQueue(SceDmaOp*);
+SceDmaOp *sceKernelDmaOpAlloc(void);
+s32 sceKernelDmaOpFree(SceDmaOp*);
 int DmacManForKernel_E18A93A5(void*, void*);
 
